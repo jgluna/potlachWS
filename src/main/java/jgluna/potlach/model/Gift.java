e87772cbd7eb11833d8836e2c0a63c045fc97fe4
Juan Luna <jgluna85@gmail.com>
2015-04-30 23:32:44 -0430

    Created Gender enum Created the content in User and Gift models

--- src/main/java/jgluna/potlach/model/Gift.java
+++ src/main/java/jgluna/potlach/model/Gift.java
@@ -1,4 +1,88 @@
 package jgluna.potlach.model;
 
+import java.util.Date;
+
 public class Gift {
+
+    private long id;
+    private Gift parentGift;
+    private String title;
+    private String text;
+    private Date creationDate;
+    private User user;
+    private String imageURL;
+    private int touchesCount;
+    private int reportCount;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Gift getParentGift() {
+        return parentGift;
+    }
+
+    public void setParentGift(Gift parentGift) {
+        this.parentGift = parentGift;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public int getTouchesCount() {
+        return touchesCount;
+    }
+
+    public void setTouchesCount(int touchesCount) {
+        this.touchesCount = touchesCount;
+    }
+
+    public int getReportCount() {
+        return reportCount;
+    }
+
+    public void setReportCount(int reportCount) {
+        this.reportCount = reportCount;
+    }
 }
--- src/main/java/jgluna/potlach/model/User.java
+++ src/main/java/jgluna/potlach/model/User.java
@@ -1,4 +1,79 @@
 package jgluna.potlach.model;
 
+import java.util.Date;
+
 public class User {
+
+    private String email;
+    private String name;
+    private String password;
+    private Gender gender;
+    private String country;
+    private Date memberSince;
+    private int reportCount;
+    private boolean isActive;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Date getMemberSince() {
+        return memberSince;
+    }
+
+    public void setMemberSince(Date memberSince) {
+        this.memberSince = memberSince;
+    }
+
+    public int getReportCount() {
+        return reportCount;
+    }
+
+    public void setReportCount(int reportCount) {
+        this.reportCount = reportCount;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setIsActive(boolean isActive) {
+        this.isActive = isActive;
+    }
 }

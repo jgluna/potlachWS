083c4daa2ddf41b1839826c587ec0bc903c6d0fb
Juan Luna <jgluna85@gmail.com>
2015-05-06 22:26:25 -0430

    Enabled Spring annotations on App.java Created H2 configuration file

--- src/main/java/jgluna/potlach/main/App.java
+++ src/main/java/jgluna/potlach/main/App.java
@@ -1,9 +1,19 @@
 package jgluna.potlach.main;
 
-public class App
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import jgluna.potlach.repository.RepositoryInterface;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableAutoConfiguration
+@EnableJpaRepositories(basePackageClasses = RepositoryInterface.class)
+@Configuration
+@EnableWebMvc
+@ComponentScan
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
